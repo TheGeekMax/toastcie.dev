3079b63d7fb14346993a783c5ba00642afa4ec16
TheGeekMax <maximesanciaume@gmail.com>
2025-01-05 21:50:51 +0100

    inital commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  app:{
+    head:{
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+          crossorigin: 'anonymous',
+        },
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  }
+})
