76ca5d022a15e2bcbba726c43bfdec47a8c4d699
TheGeekMax <maximesanciaume@gmail.com>
2025-02-13 23:25:20 +0100

    added small css for ca rule

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,3 +15,18 @@
         scroll-snap-type: y mandatory;
     }
 </style>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'Maxime Sanciaume',
+  description: 'Mon site de projets',
+  ogTitle: 'Maxime Sanciaume',
+  ogDescription: 'Mon site de projets',
+  ogImage: '/pictures/troma.jpg',
+  ogUrl: 'https://toastcie.dev',
+  twitterTitle: 'Maxime Sanciaume',
+  twitterDescription: 'Mon site de projets',
+  twitterImage: '/pictures/troma.jpg',
+  twitterCard: 'summary'
+})
+</script>
