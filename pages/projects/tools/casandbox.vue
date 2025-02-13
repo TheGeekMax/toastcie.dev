0c7bdce31f7696d1df337297787f494c76a98aea
TheGeekMax <maximesanciaume@gmail.com>
2025-02-13 18:08:19 +0100

    added sandbox v2 with dim1ca and lot of stuff

--- pages/projects/tools/casandbox.vue
+++ pages/projects/tools/casandbox.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="mega-center">
+        <div>
+            <p>1D Rule</p>
+            
+            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="btnradio" id="color_rb" autocomplete="off" @click="color_set_rainbow" checked>
+                <label class="btn btn-outline-primary" for="color_rb">Rainbow</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="color_bw" autocomplete="off" @click="color_set_bw">
+                <label class="btn btn-outline-primary" for="color_bw">B/W</label>
+            </div>
+            <p>auto remove <input type="checkbox" v-model="autoRemove" /></p>
+            <button @click="reset">clear</button>
+            <input class="d-none" type="checkbox" id="playcheckbox" v-model="play"/>
+            <label class="btn btn-primary" for="playcheckbox">  {{ playLabel }}</label>
+        </div>
         <canvas id="ca">
 
         </canvas>
-        <a href="">clear</a>
-        <a href="">free draw</a>
-        <a href="">restart</a>
-        <a href="">export</a>
+        <div>
+            <input type="number" v-model="ruleNumber" @change="calcRule" />
+            <p>run CA dim 1 <input type="checkbox" v-model="c1drun" /></p>
+            <input type="number" v-model="rules[0].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[1].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[2].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[3].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[4].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[5].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[6].value" @change="calcRuleNumber"/>
+            <input type="number" v-model="rules[7].value" @change="calcRuleNumber"/>
+        </div>
     </div>
 </template>
 
@@ -14,17 +37,37 @@
 let canvas : HTMLCanvasElement;
 let ctx : CanvasRenderingContext2D|null;
 
+let drawing = false;
+let penX = 0;
+let penY = 0;
+
 const WIDTH = 600;
 const HEIGHT = 600;
 const CELL_SIZE = 5;
-const COLOR_STEP = 0.2;
+const COLOR_STEP = 0.5;
 
 const cols = WIDTH / CELL_SIZE;
 const rows = HEIGHT / CELL_SIZE;
 
 const curHue = ref(1);
 
+
 const grid : number[][] = new Array(cols).fill(null).map(() => new Array(rows).fill(0));
+
+const ca : number[] = new Array(cols).fill(0);
+
+const ruleNumber : Ref<number> = ref(126);
+const play : Ref<boolean> = ref(true);
+const c1drun : Ref<boolean> = ref(false);
+const autoRemove : Ref<Boolean> = ref(false);
+const rules : Ref<number>[] = [
+    ref(0), ref(0), ref(0), ref(0), ref(0), ref(0), ref(0), ref(0)
+];
+
+const color_rainbow : Ref<boolean> = ref(true);
+
+//labels
+const playLabel = computed(() => play.value ? "play" : "pause");
 
 onMounted(() => {
     canvas = document.getElementById("ca") as HTMLCanvasElement;
@@ -46,41 +89,153 @@ onMounted(() => {
     canvas.addEventListener("mousedown", (e) => {
         e.preventDefault();
         const draw = (event: MouseEvent) => {
-            const x = Math.floor(event.offsetX / CELL_SIZE);
-            const y = Math.floor(event.offsetY / CELL_SIZE);
-
-            if (x < 0 || x >= cols || y < 0 || y >= rows) {
-                return;
-            }
-
-            grid[x][y] = curHue.value;
-            curHue.value += COLOR_STEP;
-
-            if(curHue.value > 360) curHue.value = 1;
+            penX = event.offsetX;
+            penY = event.offsetY;
         };
 
         draw(e as MouseEvent);
 
         const stopDrawing = () => {
+            drawing = false;
             canvas.removeEventListener("mousemove", draw);
             window.removeEventListener("mouseup", stopDrawing);
         };
-
+        drawing = true;
+        penX = e.offsetX;
+        penY = e.offsetY;
         canvas.addEventListener("mousemove", draw);
         window.addEventListener("mouseup", stopDrawing);
     });
-
+    
+    let frame = 0;
+    calcRule();
     setInterval(() => {
-        step();
         clear();
         showGrid();
+
+        if(!play.value){
+            return;
+        }
+
+        d2step();
+        if(c1drun.value && frame % 2  == 1){
+            d1step();
+            copyond2();
+            frame = -1;
+        }
+        frame ++;
     }, 20);
+
+    setInterval(() => {
+        draw_pixel();
+    })
 })
+
+function draw_pixel(){
+    if(!ctx){
+        return;
+    }
+
+    if(!drawing){
+        return;
+    }
+
+    const x = Math.floor(penX / CELL_SIZE);
+    const y = Math.floor(penY / CELL_SIZE);
+
+    if (x < 0 || x >= cols || y < 0 || y >= rows) {
+        return;
+    }
+
+    grid[x][y] = curHue.value;
+    curHue.value += COLOR_STEP;
+
+    if(curHue.value > 360) curHue.value = 1;
+}
+
+//for radiobutton handling
+
+function color_set_rainbow(){
+    color_rainbow.value = true;
+}
+
+function color_set_bw(){
+    color_rainbow.value = false;
+}
+
+//for 1D CA calcs
+
+function calcRule(){
+    let rule = ruleNumber.value;
+    for(let i = 0; i < 8; i++){
+        rules[i].value = rule % 2;
+        rule = Math.floor(rule / 2);
+    }
+}
+
+function calcRuleNumber(){
+    let rule = 0;
+    for(let i = 7; i >= 0; i--){
+        rule *= 2;
+        rule += rules[i].value;
+    }
+    ruleNumber.value = rule;
+    console.log(rule);
+}
+
+function getCell(x: number ): number{
+    if(x < 0 || x >= cols){
+        return 0;
+    }
+
+    return ca[x]
+}
+
+function getRuleNumber(x:number):number{
+    return getCell(x-1)*4 + getCell(x)*2 + getCell(x+1);
+}
+
+function getNextCell(x:number):number{
+    return rules[getRuleNumber(x)].value;
+}
+
+function d1step(){
+    const newCA : number[] = new Array(cols).fill(0);
+
+    for(let i = 0; i < cols; i++){
+        newCA[i] = getNextCell(i);
+    }
+
+    for(let i = 0; i < cols; i++){
+        ca[i] = newCA[i];
+    }
+}
+
+function copyond2(){
+    for(let i = 0; i < cols; i++){
+        grid[i][0] = ca[i]*curHue.value;
+    }
+    curHue.value += COLOR_STEP;
+    if(curHue.value > 360) curHue.value = 1;
+}
+
+
+// for 2D CA
 
 function fillgrid(){
     for(let i = 0; i < cols; i++){
         for(let j = 0; j < rows; j++){
-            grid[i][j] = Math.floor(0); // TODO : change to color
+            grid[i][j] = Math.floor(0);
+        }
+    }
+
+    ca[ Math.floor(cols / 2)] = 1;
+}
+
+function reset(){
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j] = Math.floor(0);
         }
     }
 }
@@ -94,7 +249,7 @@ function clear(){
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
 
-function step(){
+function d2step(){
     //step 0 : create a new grid
     const newGrid : number[][] = new Array(cols).fill(null).map(() => new Array(rows).fill(0));
 
@@ -132,6 +287,22 @@ function step(){
     }
 }
 
+function shift(){
+    const newGrid : number[][] = new Array(cols).fill(null).map(() => new Array(rows).fill(0));
+    
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows-1; j++){
+            newGrid[i][j+1] = grid[i][j];
+        }
+    }
+    
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j] = newGrid[i][j];
+        }
+    }
+}
+
 function isEmpty(x : number, y : number, otherGrid:number[][]) : boolean{
     if(x < 0 || x >= cols || y < 0 || y >= rows){
         return false;
@@ -144,6 +315,8 @@ function showGrid(){
         return;
     }
 
+    let full = true;
+
     for(let i = 0; i < cols; i++){
         for(let j = 0; j < rows; j++){
             const x = i * CELL_SIZE;
@@ -151,37 +324,31 @@ function showGrid(){
 
             if(grid[i][j] != 0){
                 //let [r,g,b] = HSVtoRGB(120,100,100)
-                ctx.fillStyle = 'hsl(' + grid[i][j] + ',100%,50%)'
+                if(color_rainbow.value){
+                    ctx.fillStyle = 'hsl(' + grid[i][j] + ',100%,50%)'
+                }else{
+                    ctx.fillStyle = 'white';
+                }
                 ctx.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+            }
+            if(j >= rows-5 && grid[i][j] == 0){
+                full = false;
             }
         }
     }
-}
 
-function HSVtoRGB(h : number, s : number, v : number) {
-    var r, g, b, i, f, p, q, t;
-
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        default: r = v, g = p, b = q; break; // case 5
+    if(autoRemove.value && full){
+        shift();
     }
-    return [
-        Math.round(r * 255),
-        Math.round(g * 255),
-        Math.round(b * 255)
-    ]
 }
 </script>
 
 <style lang="css" scoped>
-
+.mega-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+}
 </style>
